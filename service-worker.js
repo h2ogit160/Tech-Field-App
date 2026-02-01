58177447b2b7079a1497f8b8702e1057adf284a2
h2ogit160 <h2oeasy@gmail.com>
2026-02-01 18:49:48 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,31 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("tech-field-v12.4").then((cache) =>
-      cache.addAll(["./index.html"])
-    )
-  );
+const CACHE = "tech-field-v12.4-pwa";
+const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : Promise.resolve())))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return; // never interfere with submissions
+
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((hit) => hit || caches.match("./index.html")))
   );
 });
